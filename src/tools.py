3b1b25f6b67b31cbc1f3bd118e0c97bb9f4cb9ae
lyra-poing-serre <dupont.axs@outlook.fr>
2025-03-24 14:26:48 +0100

    début split node with markdown

--- src/tools.py
+++ src/tools.py
@@ -41,8 +41,39 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
     return new_nodes
 
+
 def extract_markdown_images(text):
     return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
+
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        matches = extract_markdown_images(old_node.text)
+        if not matches:
+            new_nodes.append(old_node)
+
+        split_nodes = []
+        for image_alt, image_link in matches:
+            sections = old_node.text.split(f"![{image_alt}]({image_link})", 1)
+            if len(matches) == 1:
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+                new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+                new_nodes.append(TextNode(sections[-1], TextType.TEXT))
+            for i in range(len(sections)):
+                if i % 2 == 0:
+                    split_nodes.append(TextNode(sections[i], TextType.TEXT))
+                else:
+                    split_nodes.append(TextNode(sections[i], TextType.IMAGE))
+        
+        for node in split_nodes: # supression du match précédent ou suivant qui été pas dans le split.
+            if not extract_markdown_images(node.text):
+                new_nodes.append(node)
+    return new_nodes
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,7 +1,7 @@
 import unittest
 from src.textnode import TextType, TextNode
 from src.htmlnode import LeafNode
-from src.tools import extract_markdown_images, extract_markdown_links, text_node_to_html_node, split_nodes_delimiter
+from src.tools import extract_markdown_images, extract_markdown_links, split_nodes_image, text_node_to_html_node, split_nodes_delimiter
 from collections import OrderedDict
 
 
@@ -151,3 +151,23 @@ class TestExtractMarkdown(unittest.TestCase):
             "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
         )
         self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
+
+
+class TestSplitNodes(unittest.TestCase):
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
