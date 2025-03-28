ef3cdc04c76cdfbbe6b3fd8d3ef12ba597fc3a9b
Lyra <dupont.axs@outlook.fr>
2025-03-28 10:34:40 +0100

    auto indent...

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -291,11 +291,11 @@ class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks_heading(self):
         md = """
     # This is a heading
-    
-        ## This a second heading
-        
+
+        ## This is a second heading
+
         This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
-        
+
         - This is the first list item in a list block
         - This is a list item
         - This is another list item
