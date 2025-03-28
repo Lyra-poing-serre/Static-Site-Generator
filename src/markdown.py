cbc0ae91ce04cc9832314563cd2a843e6ef35fd3
Lyra <dupont.axs@outlook.fr>
2025-03-28 11:14:20 +0100

    block to blocktype

--- src/markdown.py
+++ src/markdown.py
@@ -1,3 +1,4 @@
+import re
 from enum import Enum
 
 
@@ -17,5 +18,15 @@ def markdown_to_blocks(markdown: str) -> list:
     return ["\n".join(list(map(str.strip, block.split('\n')))) for block in blocks]  # REMOVE trailing whitespace
 
 
-def block_to_block_type(block: str):
-    pass
+def block_to_block_type(block: str) -> BlockType:
+    if re.match(r'^#{1,6} ', block):
+        return BlockType.HEADING
+    elif block.startswith('```') and block.endswith('```'):
+        return BlockType.CODE
+    elif block.startswith('>'):
+        return BlockType.QUOTE
+    elif block.startswith('- '):
+        return BlockType.UNORDERED_LIST
+    elif block.startswith('1. '):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -24,34 +24,75 @@ class TestMarkdownToBlocks(unittest.TestCase):
             ],
         )
 
+    def test_markdown_to_blocks_heading(self):
+        md = """
+    # This is a heading
+    
+        ## This is a second heading
+    
+        This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
+    
+        - This is the first list item in a list block
+        - This is a list item
+        - This is another list item
+    """
+        blocks = markdown_to_blocks(md)
+        self.assertListEqual(
+            blocks,
+            [
+                "# This is a heading",
+                "## This is a second heading",
+                "This is a paragraph of text. It has some **bold** and _italic_ words inside of it.",
+                "- This is the first list item in a list block\n- This is a list item\n- This is another list item"
+            ],
+        )
 
-def test_markdown_to_blocks_heading(self):
-    md = """
-# This is a heading
+    def test_markdown_to_blocks_empty_markdown(self):
+        md = ""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(blocks, [""])
+        with self.assertRaises(TypeError):
+            markdown_to_blocks(None)
 
-    ## This is a second heading
 
-    This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
+class TestBlockToMarkdownType(unittest.TestCase):
+    def test_block_to_block_type(self):
+        md = """
+# This is an H1 Heading
+## This is an H2 Heading
+### This is an H3 Heading
 
-    - This is the first list item in a list block
-    - This is a list item
-    - This is another list item
+This is a paragraph in Markdown. It can span multiple lines and will be rendered as a single block of text.
+
+```python 
+inline code
+```
+
+> This is a blockquote.
+> It can span multiple lines.
+
+- Item 1
+- Item 2
+  - Subitem 2.1
+  - Subitem 2.2
+- Item 3
+
+1. First item
+2. Second item
+   1. Subitem 2.1
+   2. Subitem 2.2
+3. Third item
 """
-    blocks = markdown_to_blocks(md)
-    self.assertListEqual(
-        blocks,
-        [
-            "# This is a heading",
-            "## This is a second heading",
-            "This is a paragraph of text. It has some **bold** and _italic_ words inside of it.",
-            "- This is the first list item in a list block\n- This is a list item\n- This is another list item"
-        ],
-    )
-
-
-def test_markdown_to_blocks_empty_markdown(self):
-    md = ""
-    blocks = markdown_to_blocks(md)
-    self.assertEqual(blocks, [""])
-    with self.assertRaises(TypeError):
-        markdown_to_blocks(None)
+        blocks = markdown_to_blocks(md)
+        blocks_type = list(map(block_to_block_type, blocks))
+        self.assertListEqual(
+            [
+                BlockType.HEADING,
+                BlockType.PARAGRAPH,
+                BlockType.CODE,
+                BlockType.QUOTE,
+                BlockType.UNORDERED_LIST,
+                BlockType.ORDERED_LIST,
+            ],
+            blocks_type
+        )
