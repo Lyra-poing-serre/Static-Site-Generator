6eac80f6241d0697d187870990e446f7c552309d
Lyra <dupont.axs@outlook.fr>
2025-04-02 13:10:20 +0200

    base path

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ from os.path import dirname, abspath
 
 from src.tools import copy_static_to_public, copy_content_dir
 
-root_dir = Path(dirname(abspath(__file__)))
+
+def main(base_path: str) -> None:
+    root_dir = Path(dirname(abspath(__file__)))
+    copy_static_to_public(root_dir)
+
+    copy_content_dir(
+        from_path=root_dir / 'content',
+        dest_path=root_dir / 'public',
+        template_path=root_dir / 'template.html',
+        base_path=base_path
+    )
+
 
 if __name__ == "__main__":
-    base_path = sys.argv[0]
-    print(sys.argv)
-    if not base_path:
-        base_path = '.'
-    base_path = Path(base_path).resolve()
-    # copy_static_to_public(base_path)
-#
-    # copy_content_dir(
-    #     base_path / 'content',
-    #     base_path / 'template.html',
-    #     base_path / 'public',
-    # )
+    base_path = sys.argv[-1]
+    if not base_path or 'main.py' in base_path:
+        base_path = '/'
+    main(base_path)
--- src/tools.py
+++ src/tools.py
@@ -36,7 +36,7 @@ def copy_static_to_public(root_dir):
     copy_source()
 
 
-def copy_content_dir(from_path: Path, template_path: Path, dest_path: Path):
+def copy_content_dir(*, from_path: Path, dest_path: Path, template_path: Path, base_path: str):
     template = template_path.read_text()
 
     def generate_page(source_dir: Path, destination_dir: Path):
@@ -48,7 +48,11 @@ def copy_content_dir(from_path: Path, template_path: Path, dest_path: Path):
                 markdown = item.read_text()
                 nodes = markdown_to_html_node(markdown)
                 title = extract_title(markdown)
-                new_page = template.replace("{{ Title }}", title).replace("{{ Content }}", nodes.to_html())
+                new_page = template.replace(
+                    "{{ Title }}", title
+                ).replace("{{ Content }}", nodes.to_html()
+                          ).replace('href="/', f'href="{base_path}'
+                                    ).replace('src="/', f'src="{base_path}')
 
                 target_path.parent.mkdir(parents=True, exist_ok=True)
                 target_path.touch()
