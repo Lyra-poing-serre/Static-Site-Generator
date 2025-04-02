34cd719b747cd69c47bb7de1404db807ce933cc0
Lyra <dupont.axs@outlook.fr>
2025-04-02 12:57:06 +0200

    TEST BASE PATH LINUX

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
+import sys
 from pathlib import Path
 from os.path import dirname, abspath
 
 from src.tools import copy_static_to_public, copy_content_dir
 
-
-def main() -> None:
-    root_dir = Path(dirname(abspath(__file__)))
-    copy_static_to_public(root_dir)
-
-    copy_content_dir(
-        root_dir / 'content',
-        root_dir / 'template.html',
-        root_dir / 'public',
-    )
-
+root_dir = Path(dirname(abspath(__file__)))
 
 if __name__ == "__main__":
-    main()
+    base_path = sys.argv[0]
+    print(sys.argv)
+    if not base_path:
+        base_path = '.'
+    base_path = Path(base_path).resolve()
+    # copy_static_to_public(base_path)
+#
+    # copy_content_dir(
+    #     base_path / 'content',
+    #     base_path / 'template.html',
+    #     base_path / 'public',
+    # )
