d25b9a19fe5a636eaceda06cdf6b3d87b2555c5d
Lyra <dupont.axs@outlook.fr>
2025-04-02 12:02:49 +0200

    ...

--- content/custom.py
+++ content/custom.py
@@ -8,13 +8,13 @@ DIRECTORY = "."
 class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
-            self.path = '/template.html'
+            self.path = '../template.html'
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 
 Handler = CustomHTTPRequestHandler
 Handler.directory = DIRECTORY
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving HTTP on 0.0.0.0 port {PORT} (http://0.0.0.0:{PORT}/) ...")
+with socketserver.TCPServer(("127.0.0.1", PORT), Handler) as httpd:
+    print(f"Serving HTTP on 127.0.0.1 port {PORT} (http://127.0.0.1:{PORT}/) ...")
     httpd.serve_forever()
